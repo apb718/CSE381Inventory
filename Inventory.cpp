5dd7fd9da177f4d9d5617e29bd71ba385139dbb3
BuildTools <unconfigured@null.spigotmc.org>
2024-02-27 10:46:15 -0600

    Add inventory file

---
+++ Inventory.cpp
@@ -0,0 +1,54 @@
+#include "Inventory.h"
+#include <iostream>
+
+Inventory::Inventory(int id, const std::string& name, int quantity, double price) {
+    this->itemID = id;
+    this->itemName = name;
+    this->quantity = quantity;
+    this->price = price;
+}
+
+// Parameterized constructor
+Inventory::Inventory() {
+    this->itemID = 0;
+    this->itemName = "";
+    this->quantity = 0;
+    this->price = 0.0;
+}
+// Getter and setter functions
+int Inventory::getItemID() const {
+    return itemID;
+}
+
+void Inventory::setItemID(int id) {
+    itemID = id;
+}
+
+const std::string& Inventory::getItemName() const {
+    return itemName;
+}
+
+void Inventory::setItemName(const std::string& name) {
+    itemName = name;
+}
+
+int Inventory::getQuantity() const {
+    return quantity;
+}
+
+void Inventory::setQuantity(int qty) {
+    quantity = qty;
+}
+
+double Inventory::getPrice() const {
+    return price;
+}
+
+void Inventory::setPrice(double itemPrice) {
+    price = itemPrice;
+}
+
+// Display item details
+void Inventory::displayItemDetails() const {
+    std::cout << "Item ID: " << std::endl;
+}
